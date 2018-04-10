51f4649c70d5685190fb94cca7125eb0c8af1870
Santiago Sánchez García <santiago.sanchez@webfg.com>
2018-04-10 11:01:28 +0200

    En la rama master se añadio angularfire Tu rama está actualizada con 'origin/master'.
     Cambios a ser confirmados:
    	modificado:     app/package-lock.json
    	modificado:     app/package.json
    	modificado:     app/src/app/app.module.ts
    	modificado:     app/src/pages/signup/signup.html
    	modificado:     app/src/pages/signup/signup.ts

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,9 +4,21 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { SignUpPage } from '../pages/signup/signup';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDG5bmQLBp27v60VLv1YWk5Y9pkChc2VeI',
+  authDomain: 'commando-cub-test.firebaseapp.com',
+  databaseURL: 'https://commando-cub-test.firebaseio.com',
+  storageBucket: 'commando-cub-test.appspot.com',
+  messagingSenderId: '306564613918'
+}
 
 @NgModule({
   declarations: [
@@ -16,7 +28,10 @@ import { SignUpPage } from '../pages/signup/signup';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -27,6 +42,8 @@ import { SignUpPage } from '../pages/signup/signup';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
+    AngularFireAuth,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- app/src/pages/signup/signup.ts
+++ app/src/pages/signup/signup.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html'
 })
 export class SignUpPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+  	public navCtrl: NavController,
+  	private afAuth: AngularFireAuth
+  ) { }
 
+  signUpEmailPassword () {
+  	console.log(this.afAuth);
   }
-
 }
