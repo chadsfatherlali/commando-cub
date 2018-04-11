611bc2a2bbc35756ec1da183e2f76405b79f2200
Santiago Sánchez García <santiago.sanchez@webfg.com>
2018-04-11 10:42:28 +0200

    En la rama master se añadios proveedor de facebook Tu rama está actualizada con 'origin/master'. Cambios a ser confirmados: 	modificado:     app/src/app/app.scss 	modificado:     app/src/pages/home/home.html 	modificado:     app/src/pages/signup/signup.html 	modificado:     app/src/pages/signup/signup.ts 	modificado:     init.sh

--- app/src/pages/signup/signup.ts
+++ app/src/pages/signup/signup.ts
@@ -1,20 +1,62 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html'
 })
 export class SignUpPage {
+	private signUpWithUpEmailPassword_form : FormGroup
+	private fbProvider : any = new firebase.auth.FacebookAuthProvider()
+	private errorSignup : any = null
+	private userCredentials : any = {
+		email: null,
+		password: null
+	}
 
   constructor(
   	public navCtrl: NavController,
-  	private afAuth: AngularFireAuth
-  ) { }
+  	private afAuth: AngularFireAuth,
+  	private formBuilder: FormBuilder
+  ) {
+  	this.signUpWithUpEmailPassword_form = this.formBuilder.group({
+  		email: [
+  			'', 
+  			[
+  				Validators.required, 
+  				Validators.email
+  			]
+  		],
+  		password: [
+  			'', 
+  			[
+  				Validators.required, 
+  				Validators.minLength(6)
+  			]
+  		]
+  	})
+  }
 
-  signUpEmailPassword () {
-  	console.log(this.afAuth);
+  private signUpWithFacebook () {
+  	this.fbProvider.setCustomParameters({
+  		'display': 'popup'
+  	})
+
+  	this.afAuth.auth.signInWithPopup(this.fbProvider)
+  }
+
+  private signUpWithUpEmailPassword () {
+  	this.afAuth.auth.createUserWithEmailAndPassword(this.userCredentials.email, this.userCredentials.password)
+  	.then(res => {
+  		this.userCredentials.email = null
+  		this.userCredentials.password = null
+  		this.errorSignup = null
+  	})
+  	.catch(err => {
+  		this.errorSignup = err.code
+  	})
   }
 }
--- init.sh
+++ init.sh
@@ -3,4 +3,6 @@
 chown -R 1000:1000 /code/app
 cd /code/app
 
+npm install
+
 ionic serve -c --no-interactive
